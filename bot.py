bca0fc917791091d6dbef9ebe0545103907a7b7d
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-07-22 18:07:46 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from config import BOT_TOKEN
+from database import create_table
+
+from handlers.start import router as start_router
+from handlers.admin import router as admin_router
+
+
+bot = Bot(
+    token=BOT_TOKEN
+)
+
+dp = Dispatcher()
+
+
+dp.include_router(start_router)
+dp.include_router(admin_router)
+
+
+async def main():
+
+    create_table()
+
+    print("🦅 Орёл VPN бот запущен")
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
