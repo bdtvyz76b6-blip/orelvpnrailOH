fcf1723080f93ad17302b43877ec83080615f416
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-07-22 16:13:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,23 +5,23 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from database import create_table
 
+# Подключаем хендлеры
 from handlers.start import router as start_router
 from handlers.admin import router as admin_router
-from handlers.payments import router as payments_router
+# from handlers.payments import router as payments_router
 
 
 bot = Bot(token=BOT_TOKEN)
-
 dp = Dispatcher()
 
 
+# Подключаем роутеры
 dp.include_router(start_router)
-dp.include_router(payments_router)
 dp.include_router(admin_router)
+# dp.include_router(payments_router)
 
 
 async def main():
-
     create_table()
 
     print("🦅 Орёл VPN бот запущен")
