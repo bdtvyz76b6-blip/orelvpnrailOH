df13fda9db0027555e9195bdc1cd862421137bb8
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-07-26 14:31:39 +0300

    Create admin_panel.py

---
+++ handlers/admin_panel.py
@@ -0,0 +1,35 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+from config import ADMIN_ID
+from keyboards import admin_menu
+
+
+router = Router()
+
+
+# =====================
+# АДМИН ПАНЕЛЬ
+# =====================
+
+@router.message(Command("admin"))
+async def admin_start(message: Message):
+
+    if message.from_user.id != ADMIN_ID:
+
+        await message.answer(
+            "❌ Нет доступа."
+        )
+
+        return
+
+
+    await message.answer(
+        """
+🦅 Админ панель
+
+Выберите действие:
+""",
+        reply_markup=admin_menu()
+    )
