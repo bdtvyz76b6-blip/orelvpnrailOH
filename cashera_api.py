12423be7397b021a16d5f7ec69caf9b7ca7150bc
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-07-28 16:58:22 +0300

    Update cashera_api.py

--- cashera_api.py
+++ cashera_api.py
@@ -1,10 +1,15 @@
 import os
 import requests
+import uuid
 
 
 CASHERA_API_KEY = os.getenv(
     "CASHERA_API_KEY"
 )
+
+
+BASE_URL = "https://api.cashera.cash/api/v1"
+
 
 
 def create_cashera_payment(
@@ -13,46 +18,81 @@ def create_cashera_payment(
     days
 ):
 
-    url = "https://api.cashera.cash/api/v1/integration/transactions"
+    url = (
+        f"{BASE_URL}/integration/transactions"
+    )
 
 
     headers = {
+
         "X-Api-Key": CASHERA_API_KEY,
+
         "Content-Type": "application/json"
+
     }
+
+
+
+    # уникальный номер заказа
+    order_id = (
+        f"{user_id}_{uuid.uuid4().hex}"
+    )
+
 
 
     data = {
 
-        # сумма в копейках
+        # рубли -> копейки
         "amount": amount * 100,
+
 
         "currency": "RUB",
 
+
         "payment_method": "sbp",
 
-        # сюда кладём Telegram ID
-        "external_id": str(user_id),
 
-        "description": f"Орёл VPN — {days} дней",
+        # уникальный заказ
+        "external_id": order_id,
+
+
+        "description":
+            f"Орёл VPN — {days} дней",
+
 
         "callback_url":
-        "https://orelvpnrailoh-production.up.railway.app/webhook/cashera"
+            "https://orelvpnrailoh-production.up.railway.app/webhook/cashera",
+
+
+        "success_url":
+            "https://t.me/orelvpntopbot",
+
+
+        "fail_url":
+            "https://t.me/orelvpntopbot"
 
     }
 
 
+
     response = requests.post(
+
         url,
+
         headers=headers,
-        json=data
+
+        json=data,
+
+        timeout=15
+
     )
 
 
     print(
-        "CASHeRA CREATE:",
+        "💳 CASHeRA CREATE:",
         response.text
     )
+
 
 
     return response.json()
