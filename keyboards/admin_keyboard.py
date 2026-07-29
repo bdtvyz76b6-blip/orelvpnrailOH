c411d6b70ff4ae08c309950681d3f4e6049059ea
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-07-29 17:07:50 +0300

    Create admin_keyboard.py

---
+++ keyboards/admin_keyboard.py
@@ -0,0 +1,39 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def admin_menu():
+
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="👥 Пользователи",
+                    callback_data="admin_users"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="💳 Платежи",
+                    callback_data="admin_payments"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="🎟 Промокоды",
+                    callback_data="admin_promos"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="📢 Рассылка",
+                    callback_data="admin_broadcast"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="📊 Статистика",
+                    callback_data="admin_stats"
+                )
+            ]
+        ]
+    )
