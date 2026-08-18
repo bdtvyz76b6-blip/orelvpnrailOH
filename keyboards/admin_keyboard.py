9570947e94b61e446cda8055ae64cd0d785705d8
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-08-19 00:05:43 +0300

    Update admin_keyboard.py

--- keyboards/admin_keyboard.py
+++ keyboards/admin_keyboard.py
@@ -1,51 +1,62 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
 
 
 def admin_menu():
 
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="👥 Пользователи",
-                    callback_data="admin_users"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="🔎 Найти пользователя",
-                    callback_data="admin_search"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="💳 Платежи",
-                    callback_data="admin_payments"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="🎟 Промокоды",
-                    callback_data="admin_promos"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="📢 Рассылка",
-                    callback_data="admin_broadcast"
-                )
-            ],
+
             [
                 InlineKeyboardButton(
                     text="📊 Статистика",
                     callback_data="admin_stats"
                 )
             ],
+
+            [
+                InlineKeyboardButton(
+                    text="👥 Пользователи",
+                    callback_data="admin_users"
+                )
+            ],
+
+            [
+                InlineKeyboardButton(
+                    text="🔎 Найти пользователя",
+                    callback_data="admin_search"
+                )
+            ],
+
+            [
+                InlineKeyboardButton(
+                    text="💳 Платежи",
+                    callback_data="admin_payments"
+                )
+            ],
+
+            [
+                InlineKeyboardButton(
+                    text="🎟 Промокоды",
+                    callback_data="admin_promos"
+                )
+            ],
+
+            [
+                InlineKeyboardButton(
+                    text="📢 Рассылка",
+                    callback_data="admin_broadcast"
+                )
+            ],
+
             [
                 InlineKeyboardButton(
                     text="🔄 Обновить серверы",
                     callback_data="admin_sync_servers"
                 )
             ]
+
         ]
     )
