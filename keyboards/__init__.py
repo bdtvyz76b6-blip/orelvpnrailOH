7cf591f33d027731591b83505fd1fb3b7bc2c2da
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-08-19 00:05:24 +0300

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,22 @@
- 
+from .user_keyboard import (
+    main_menu,
+    payment_method_keyboard,
+    stars_buy_keyboard,
+    sbp_buy_keyboard,
+    cabinet_keyboard,
+    accept_terms_keyboard
+)
+
+from .admin_keyboard import (
+    admin_menu
+)
+
+__all__ = [
+    "main_menu",
+    "payment_method_keyboard",
+    "stars_buy_keyboard",
+    "sbp_buy_keyboard",
+    "cabinet_keyboard",
+    "accept_terms_keyboard",
+    "admin_menu"
+]
