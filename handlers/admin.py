94f992d0ccf080a100bf74283c57756d64c983ce
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-08-18 23:35:04 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,18 +1,36 @@
-from aiogram import Router, F
+from aiogram import Router
 from aiogram.types import Message
+from aiogram.filters import Command
 
-from keyboards.admin_keyboard import admin_menu
 from config import ADMIN_IDS
+from keyboards.admin_keyboard import admin_menu
 
 
 router = Router()
 
 
-@router.message(F.text == "/admin")
-async def admin(message: Message):
-    # Проверяем, является ли пользователь админом
+# =====================
+# АДМИН ПАНЕЛЬ
+# =====================
+
+@router.message(Command("admin"))
+async def admin_start(message: Message):
+
+    # =====================
+    # ПРОВЕРКА АДМИНА
+    # =====================
+
     if message.from_user.id not in ADMIN_IDS:
+
+        await message.answer(
+            "❌ Нет доступа."
+        )
+
         return
+
+    # =====================
+    # АДМИН МЕНЮ
+    # =====================
 
     await message.answer(
         "🦅 Админ-панель\n\n"
